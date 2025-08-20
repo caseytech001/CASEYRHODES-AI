bff851cd891040272ba0d2ad0017665c5420c31f
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-08-20 13:25:06 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴛᴇᴄʜ

--- data/call-handler.js
+++ data/call-handler.js
@@ -2,10 +2,10 @@ import config from '../config.cjs';
 
 const Callupdate = async (json, sock) => {
    for (const id of json) {
-      if (id.status === 'offer' && config.REJECT_CALL ) {
+      if (id.status === 'offer' && config.REJECT_CALL) {
          let msg = await sock.sendMessage(id.from, {
             text: `*_📞 Auto Reject Call Mode Activated_* \n*_📵 No Calls Allowed_*`,
-            mentions: [id.from],
+            mentions: [id.from]
          });
          await sock.rejectCall(id.id, id.from);
       }
