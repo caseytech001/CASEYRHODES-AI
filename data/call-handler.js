4a0ec3e47c85157780178a6cb16498f7b30b2ee2
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-08-22 13:18:35 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴛᴇᴄʜ

--- data/call-handler.js
+++ data/call-handler.js
@@ -4,14 +4,8 @@ const Callupdate = async (json, sock) => {
    for (const id of json) {
       if (id.status === 'offer' && config.REJECT_CALL) {
          let msg = await sock.sendMessage(id.from, {
-            text: `*ICall Manager*\n\n📵 *Auto Call Rejection Enabled*\n\nType .list to see all commands\n\n© CASEYRHODES-AI | ${new Date().getFullYear()}`,
-            footer: 'Commands  Ping  GitHub  YouTube  Telegram',
-            templateButtons: [
-               { index: 1, urlButton: { displayText: '⭐ GitHub', url: 'https://github.com/caseyweb' } },
-               { index: 2, urlButton: { displayText: '📺 YouTube', url: 'https://youtube.com/caseyrhodes01' } },
-               { index: 3, urlButton: { displayText: '📱 Telegram', url: 'https://t.me/caseyrhodes001' } },
-               { index: 4, quickReplyButton: { displayText: '.list', id: 'list_command' } }
-            ]
+            text: `*_📞 Auto Reject Call Mode Activated_* \n*_📵 No Calls Allowed_*`,
+            mentions: [id.from]
          });
          await sock.rejectCall(id.id, id.from);
       }
