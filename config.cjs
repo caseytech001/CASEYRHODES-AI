d65a5b979226d2d7ab416555cc45a92fad131bd0
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-08-25 22:50:45 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴛᴇᴄʜ

--- config.cjs
+++ config.cjs
@@ -22,6 +22,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
+  AUTO_REACT: process.env.AUTO_REACT || "true",
+// make this true or false for auto react on all msgs
   BOT_NAME: process.env.BOT_NAME || "JINX-MD",
   MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/6852sf.jpg",
   DESCRIPTION: process.env.DESCRIPTION || "© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴛᴇᴄʜ",
