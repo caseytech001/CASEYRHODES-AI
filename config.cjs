47c10ffdfd6b1a897b142c4851bdcd8211d141de
caseytech001 <caseytech666@gmail.com>
2025-08-31 13:57:41 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -22,7 +22,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  AUTO_REACT: process.env.AUTO_REACT || "true",
+  AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
   BOT_NAME: process.env.BOT_NAME || "JINX-MD",
   MENU_IMAGE: process.env.MENU_IMAGE || "https://i.ibb.co/wZ4ypv7Y/caseytech.jpg",
